e0c2c3e0c48952a6bdcb1fdd5f8bd85da546d126
Eduardo <edomin11@ucsc.edu>
2025-08-04 11:26:45 -0700

    Rough draft for HTML, CSS, copied from dashboard to start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddStoryComponent } from "./add-story/add-story.component";
 
 @Component({
   selector: 'app-root',
   //imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [AddStoryComponent]
 })
 export class AppComponent {
-  title = 'fico-intern-dashboard';
+  title = 'add-story-page';
 }
