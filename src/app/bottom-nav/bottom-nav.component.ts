9a8f3ceff35b9edfb0af0165f304e19bcaa3ce4d
Mohamed HOsh <mohamedhosh01@gmail.com>
2025-08-04 23:57:28 -0700

    Added icon to the bottom navigation

--- src/app/bottom-nav/bottom-nav.component.ts
+++ src/app/bottom-nav/bottom-nav.component.ts
@@ -1,8 +1,11 @@
+
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-bottom-nav',
-  imports: [],
+  imports: [RouterModule, MatIconModule],
   templateUrl: './bottom-nav.component.html',
   styleUrl: './bottom-nav.component.css'
 })
