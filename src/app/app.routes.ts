5e00d056a47db0b8f77fc28b6060017647498cb2
Mohamed HOsh <mohamedhosh01@gmail.com>
2025-08-06 00:14:47 -0700

    Add the routes to the add-story page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { BlankPageComponent } from './blank-page/blank-page.component';
 import { HomeDashboardComponent } from './home-dashboard/home-dashboard.component';
+import { AddStoryComponent } from './add-story/add-story.component';
 
 export const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   { path: 'blank', component: BlankPageComponent },
   { path: 'home', component: HomeDashboardComponent },
+  { path: 'add-story', component: AddStoryComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
 ];
