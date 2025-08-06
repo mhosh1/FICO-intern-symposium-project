555c62d97705497794aa6b5d2456f7160248ee53
Mohamed HOsh <mohamedhosh01@gmail.com>
2025-08-05 23:41:56 -0700

    Completed the routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
-
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { BlankPageComponent } from './blank-page/blank-page.component';
+import { HomeDashboardComponent } from './home-dashboard/home-dashboard.component';
 
 export const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   { path: 'blank', component: BlankPageComponent },
+  { path: 'home', component: HomeDashboardComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
 ];
